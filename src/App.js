6ec494e8f713e56426b6fa9e36c43903edb3cf04
kashif <kashif_saleem_23@hotmail.com>
2023-08-25 18:01:52 +0500

    Feat: Added the error page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import img from "./assets/not-found.png";
+import styled from "styled-components";
 
-function App() {
+const Error = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <Wrapper className="full-page">
+      <div>
+        <img src={img} alt="not found" />
+        <h3>:! Page Not Found</h3>
+        {/* <p>
+					We apologize, but it seems the page you are looking for cannot be
+					found. Please click on the Back Home button to return to our homepage.
+					Thank you for your understanding.
+				</p> */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          We apologise for the inconvinience, the server is currenlty not
+          responding. Please follow the instructions in the Readme.md file to
+          run the project locally
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        {/* <Link to="/">back home</Link> */}
+        <a href="https://github.com/Kashif-Saleem-Ghuman/Cars-Rental-FrontEnd/blob/develop/README.md">
+          Readme
         </a>
-      </header>
-    </div>
+      </div>
+    </Wrapper>
   );
-}
+};
 
-export default App;
+const Wrapper = styled.main`
+  text-align: center;
+  img {
+    width: 90vw;
+    max-width: 600px;
+    display: block;
+    margin-bottom: 2rem;
+    margin: 0 auto;
+  }
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h3 {
+    margin-bottom: 0.5rem;
+    font-size: 2rem;
+  }
+  p {
+    margin-top: 0;
+    margin-bottom: 0.5rem;
+    color: #555;
+    font-size: 1.25rem;
+  }
+  a {
+    text-decoration: underline;
+    text-transform: capitalize;
+    font-size: 1.5rem;
+  }
+`;
+
+export default Error;
